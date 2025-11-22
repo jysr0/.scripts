ee0d3bac91128502720cf0b71c1f519a373c16b4
ysr <141933879+jysr0@users.noreply.github.com>
2025-11-22 13:26:38 +0100

    Add files via upload

--- dmenu_poweroff.sh
+++ dmenu_poweroff.sh
@@ -3,10 +3,11 @@
 op1="󰐥 shutdown"
 op2="󰜉 restart"
 op3="󰤄 sleep"
+op4=" hibernate"
 ansy=" yes"
 ansn=" no"
 
-operation=$((echo $op1; echo $op2; echo $op3) | dmenu -i -p '󰐥 system shutdown:')
+operation=$((echo $op1; echo $op2; echo $op3; echo $op4) | dmenu -i -p '󰐥 system shutdown:')
 if [[ $operation == $op1 ]]; then
 	confirmation=$((echo $ansn; echo $ansy) | dmenu -i -p "󱈸 confirm '$operation':")
 	[[ $confirmation == $ansy ]] && systemctl poweroff
@@ -15,4 +16,7 @@ elif [[ $operation == $op2 ]]; then
 	[[ $confirmation == $ansy ]] && systemctl reboot
 elif [[ $operation == $op3 ]]; then
 	systemctl suspend
+elif [[ $operation == $op4 ]]; then
+	confirmation=$((echo $ansn; echo $ansy) | dmenu -i -p "󱈸 confirm '$operation':")
+	[[ $confirmation == $ansy ]] && systemctl hibernate
 fi
