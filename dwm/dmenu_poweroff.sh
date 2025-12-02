f7c9fb232bfe471430271df2e4d89bbb3f9c7cd2
ysr <141933879+jysr0@users.noreply.github.com>
2025-12-02 17:40:37 +0100

    Add files via upload

---
+++ dwm/dmenu_poweroff.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+op1="󰐥 shutdown"
+op2="󰜉 restart"
+op3="󰤄 sleep"
+op4=" hibernate"
+ansy=" yes"
+ansn=" no"
+
+operation=$((echo $op1; echo $op2; echo $op3; echo $op4) | dmenu -i -p '󰐥 system shutdown:')
+if [[ $operation == $op1 ]]; then
+	confirmation=$((echo $ansn; echo $ansy) | dmenu -i -p "󱈸 confirm '$operation':")
+	[[ $confirmation == $ansy ]] && systemctl poweroff
+elif [[ $operation == $op2 ]]; then
+	confirmation=$((echo $ansn; echo $ansy) | dmenu -i -p "󱈸 confirm '$operation':")
+	[[ $confirmation == $ansy ]] && systemctl reboot
+elif [[ $operation == $op3 ]]; then
+	systemctl suspend
+elif [[ $operation == $op4 ]]; then
+	confirmation=$((echo $ansn; echo $ansy) | dmenu -i -p "󱈸 confirm '$operation':")
+	[[ $confirmation == $ansy ]] && systemctl hibernate
+fi
---
+++ dwm/dwmb_battery.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+capacity=$(cat /sys/class/power_supply/*BAT*/capacity)
+stat=$(cat /sys/class/power_supply/*BAT*/status)
+
+if [[ $stat == "Charging" ]]; then
+	case $capacity in
+		 [0-9]) echo "󰢜 $capacity%";;
+		[1][0-9]) echo "󰂆 $capacity%";;
+		[2][0-9]) echo "󰂆 $capacity%";;
+		[3][0-9]) echo "󰂈 $capacity%";;
+		[4][0-9]) echo "󰢝 $capacity%";;
+		[5][0-9]) echo "󰂉 $capacity%";;
+		[6][0-9]) echo "󰢞 $capacity%";;
+		[7][0-9]) echo "󰂊 $capacity%";;
+		[8][0-9]) echo "󰂋 $capacity%";;
+		[9][0-9]) echo "󰂅 $capacity%";;
+		100) echo " $capacity%";;
+	esac
+fi
+
+case $capacity in
+	100) echo "󰁹 $capacity%";;
+	[9][0-9]) echo "󰁹 $capacity%";;
+	[8][0-9]) echo "󰂂 $capacity%";;
+	[7][0-9]) echo "󰂁 $capacity%";;
+	[6][0-9]) echo "󰂀 $capacity%";;
+	[5][0-9]) echo "󰁿 $capacity%";;
+	[4][0-9]) echo "󰁾 $capacity%";;
+	[3][0-9]) echo "󰁽 $capacity%";;
+	[2][0-9]) echo "•󰁼 $capacity%";;
+	[1][0-9]) echo "•󰁻 $capacity%";;
+	 [0-9]) echo "•󰁺 $capacity%";;
+esac
