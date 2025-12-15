fcb33cb834476348a5df2112158f85c1088e91ad
ysr <141933879+jysr0@users.noreply.github.com>
2025-12-15 20:07:42 +0100

    Add files via upload

--- dwm/dmenu_bg.sh
+++ dwm/dmenu_bg.sh
@@ -5,4 +5,4 @@ dir="backgrounds"
 [[ -d $XDG_CACHE_HOME ]] || (mkdir ~/.cache && notify-send "'cache' directory created")
 #bg="$(ls -A $XDG_PICTURES_DIR/$dir | dmenu -i -l 10 -p '󰸉 set background:')"
 bg="$(sxiv -q -t -o $XDG_PICTURES_DIR/$dir | awk -F "/" '{print $NF}')"
-xwallpaper --stretch $XDG_PICTURES_DIR/$dir/$bg 2> /dev/null && cp $XDG_PICTURES_DIR/$dir/$bg $XDG_CACHE_HOME/bg.jpg
+xwallpaper --stretch $XDG_PICTURES_DIR/$dir/$bg 2> /dev/null && cp $XDG_PICTURES_DIR/$dir/$bg $XDG_CACHE_HOME/bg.jpg && notify-send 'Wallpaper set'
