467a10d41bfecb41689340e02a42b75531329125
ysr <141933879+jysr0@users.noreply.github.com>
2025-12-15 15:51:56 +0100

    Add files via upload

--- dwm/random_bg.sh
+++ dwm/random_bg.sh
@@ -2,4 +2,4 @@
 
 directory="$XDG_PICTURES_DIR/backgrounds/*"
 random_pic=$(shuf -e -n1 $directory)
-xwallpaper --stretch $random_pic 2> /dev/null && cp $random_pic $XDG_CACHE_HOME/bg.jpg
+xwallpaper --stretch $random_pic 2> /dev/null && notify-send -u low 'Wallpaper randomly set!' && cp $random_pic $XDG_CACHE_HOME/bg.jpg
