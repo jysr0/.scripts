ceffa807f2e13ab67e3750b3542710e4c6a333cd
ysr <141933879+jysr0@users.noreply.github.com>
2025-12-28 19:00:11 +0100

    Add files via upload

--- dwm/dwmb_kb_layout.sh
+++ dwm/dwmb_kb_layout.sh
@@ -3,4 +3,6 @@
 # Thanks to nonpop.
 # clone and compile and set in $PATH (watch for mode bits) the `xkblayout-state` from: https://github.com/nonpop/xkblayout-state.
 
-printf '󰌌 ' ; xkblayout-state print %n | cut -b 1-3 -
+# 󰗊 󰌌 󰊿 󰘳
+
+printf '󰘳 ' ; xkblayout-state print %n | cut -b 1-3 -
